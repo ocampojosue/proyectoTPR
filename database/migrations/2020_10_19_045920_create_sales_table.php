7861abb4f545a96e8c34d1717c00985532f6fca6
ocampojosue <ocampojosue@gmail.com>
2020-10-19 01:37:41 -0400

    first commit

--- database/migrations/2020_10_19_045920_create_sales_table.php
+++ database/migrations/2020_10_19_045920_create_sales_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDetailInvoicesTable extends Migration
+class CreateSalesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateDetailInvoicesTable extends Migration
      */
     public function up()
     {
-        Schema::create('detail_invoices', function (Blueprint $table) {
+        Schema::create('sales', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateDetailInvoicesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('detail_invoices');
+        Schema::dropIfExists('sales');
     }
 }
