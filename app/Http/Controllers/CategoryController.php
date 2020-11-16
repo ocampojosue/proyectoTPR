413698da9ab374c27e6e5bee170ce4f95b38515a
ocampojosue <ocampojosue@gmail.com>
2020-11-16 18:04:46 -0400

    category crud

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,9 +14,17 @@ class CategoryController extends Controller
     public function index(){
         return view('category.index');
     }
+    public function create(){
+    }
     public function store(Request $request){
         $category = new Category();
         $category->category_name = $request->category_name;
         $category->save();
+    }
+    public function destroy($id){
+    }
+    public function edit($id){
+    }
+    public function update(Request $request,$id){
     }
 }
--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class ProviderController extends Controller
 {
-    //
+    public function index(){
+        return view('provider.index');
+    }
+    public function create(){
+    }
+    public function store(Request $request){
+        $provider = new Provider();
+        $provider->name = $request->name;
+        $provider->surname = $request->surname;
+        $provider->phone = $request->phone;
+        $provider->city = $request->city;
+        $provider->direction = $request->direction;
+        $provider->ci = $request->ci;
+        $provider->company_name = $request->company_name;
+        $provider->save();
+    }
+    public function destroy($id){
+    }
+    public function edit($id){
+    }
+    public function update(Request $request,$id){
+    }
 }
