723ef5f8b894aed8e8aabe2397f55d5d5830b693
ocampojosue <ocampojosue@gmail.com>
2020-11-24 16:45:29 -0400

    product crud

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -90,6 +90,7 @@
                                 </select>
                             </div>
                         </div>
+                        <div></div>
                         <div class="form-group row mb-0" >
                             <div class="col-md-8 offset-md-4">
                                 <input type="submit" value="Save Changes" class="btn btn-primary">
